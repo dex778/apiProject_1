69c0a265e40258812443cabda0577db1da6d30ef
Kenneth Martinez <kenneth@Kenneths-MacBook-Pro.local>
2021-03-20 08:50:14 -0500

    final revision

--- loremPicsum.js
+++ loremPicsum.js
@@ -1,57 +1,34 @@
-// let lion = document.querySelector('#imageOne')
+const wish_btn = document.getElementById('wish_btn');
+const reality_btn = document.getElementById('reality_btn');
+wish_btn.addEventListener('click', display);
+reality_btn.addEventListener('click', pic);
+    
+    function randomPhoto(){
+        fetch('https://picsum.photos/916/610?random=1')
+        .then(response => {
+            display(response)
+            console.log(response)
+            
+        })
+    }
+    
 
-//  fetch('https://picsum.photos/id/1/916/610')
-//      .then(function(response){
-//         return response.json();
-//      })
-//      .then(function(myJson){
-//         console.log(myJson)
-//             displayImage(myJson)
-//      })
+    function display(pictures){
         
-//     function displayImage(pic){
-//         console.log(pic)
-//     }
-   
-
-
-// Code working correctly
-    fetch('https://picsum.photos/916/610?random=1')
-    .then(response => {
-        display(response)
-        console.log(response)
-    }) 
-    function display(pic){
-        // console.log(pic);
-        // const picOne = pic.url;
-        document.getElementById('imageOne').src=pic.url;
+        document.getElementById('imageOne').src=pictures.url;
     }
 
-    fetch('https://picsum.photos/id/1/916/610')
-    .then(res => {
-        pic(res)
-        console.log(res)
-    }) 
+    function staticPic(){
+        fetch('https://picsum.photos/id/1/916/610')
+        .then(res => {
+            pic(res)
+            console.log(res)
+        }) 
+    }
+    
+
     function pic(pics){
-        // console.log(pic);
-        // const picOne = pic.url;
+      
         document.getElementById('imageTwo').src=pics.url;
     }
 
-
-
-    
-    // fetch('https://pokeapi.co/api/v2/pokemon/mewtwo')
-    // .then(res => {
-    //     return res.json()
-    //     // console.log(res)
-    // })
-    // .then(json => {
-    //     console.log(json)
-    //     display(json)
-    // })
-
-    // function display(mewtwo){
-    //     let pokemon = document.getElementById('pokeInfo')
-    //     pokemon.src = mewtwo.sprites.front_default
-    // }
